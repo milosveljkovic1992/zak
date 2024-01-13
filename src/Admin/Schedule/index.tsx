4f7aa47c26f8a2866622178bb9a4c0020a67f62a
Milos Veljkovic <milos.veljkovic@fuel.me>
2024-01-13 19:10:39 +0100

    project setup

---
+++ src/Admin/Schedule/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { SSchedule } from './styles';
+
+const Schedule = () => (
+  <SSchedule.Container>
+    <SSchedule.Card>
+      <p>request 1</p>
+      <p>request 2</p>
+      <p>request 3</p>
+      <h1>Schedule</h1>
+    </SSchedule.Card>
+  </SSchedule.Container>
+);
+
+export default Schedule;
---
+++ src/Admin/Schedule/styles.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import { Card } from '@mui/material';
+
+export const SSchedule = {
+  Container: styled.div`
+    width: 100;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  Card: styled(Card)`
+    width: 90%;
+    max-width: 768px;
+    box-shadow: 0px 5px 15px 2px rgba(0, 0, 0, 0.1);
+    padding: 10px;
+  `,
+};
